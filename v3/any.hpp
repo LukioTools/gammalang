7d954e3a3f384735f96061bc5d8c8c8694c1ab33
PikkuHurja <pikku.hurja@gmail.com>
2024-02-13 14:26:24 +0200

    quick commit -_-

--- v3/any.hpp
+++ v3/any.hpp
@@ -10,12 +10,17 @@
 #include <type_traits>
 #include <typeinfo>
 #include <unordered_map>
+#include <utility>
 #include <vector>
 
 class Any;
 class map;
 class array;
-class string;
+class string : public std::string
+{
+public:
+    using std::string::basic_string;
+};
 typedef long integer;
 typedef double decimal;
 typedef Any(*function)(const std::span<Any>&);
@@ -25,6 +30,7 @@ class Any
 private:
     std::shared_ptr<std::any> ptr = std::make_shared<std::any>();
 public:
+    static std::unordered_map<size_t, std::unordered_map<std::string, function>> any_methods;
 
     enum AnyE{
         NONE,
@@ -36,9 +42,7 @@ public:
         FUNCTION,
     };
 
-    std::size_t hash()const{
-        return 0;
-    }
+    std::size_t hash()const;
 
     template<typename T>
     T& as()const{
@@ -50,7 +54,58 @@ public:
         }
     }
 
+
+
+    Any call(const string& name, array& span);
+    template<class ...Args>
+    Any callva(const string& name, Args ...a);
+    template<class T>
+    static void add_method(const string& name, function fn){
+        any_methods[typeid(T).hash_code()][name] = fn;
+    }
+
+    void operator=(const Any& a);
+    template<class T, typename std::enable_if<std::is_integral<T>::value, int>::type = 0>
+    void operator=(const T& a){*ptr = (integer)a;};
+    template<class T, typename std::enable_if<std::is_floating_point<T>::value, bool>::type = 0>
+    void operator=(const T& a){*ptr = (decimal)a;};
+    void operator=(const function& a);
+    void operator=(const string& a);
+    void operator=(const array& a);
+    void operator=(const map& a);
+
     bool operator==(const Any& a)const;
+    template<class T, typename std::enable_if<std::is_integral<T>::value, int>::type = 0>
+    bool operator==(const T& a)const{
+        return as<integer>() == (integer)a;
+    };
+    template<class T, typename std::enable_if<std::is_floating_point<T>::value, bool>::type = 0>
+    bool operator==(const T& a)const{
+        return as<decimal>() == (decimal)a;
+    };
+    bool operator==(const function& a)const;
+    bool operator==(const string& a)const;
+    bool operator==(const array& a)const;
+    bool operator==(const map& a)const;
+
+    operator integer(){
+        return as<integer>();
+    };
+    operator decimal(){
+        return as<decimal>();
+    };
+    operator function(){
+        return as<function>();
+    };
+    operator string&(){
+        return as<string>();
+    };
+    operator array&(){
+        return as<array>();
+    };
+    operator map&(){
+        return as<map>();
+    };
 
     Any() = default;
     template<class T, typename std::enable_if<std::is_integral<T>::value, int>::type = 0>
@@ -82,16 +137,44 @@ public:
     using std::unordered_map<Any, Any>::unordered_map;
 };
 
-class string : public std::string
-{
-public:
-    using std::string::basic_string;
-};
+
 class array : public std::vector<Any>
 {
 public:
     using std::vector<Any>::vector;
 };
+
+inline std::unordered_map<std::size_t, std::unordered_map<std::string, function>> Any::any_methods({
+    {
+        std::make_pair(
+            typeid(integer).hash_code(),
+            std::unordered_map<std::string, function>({
+                std::make_pair("==", [](const std::span<Any>& span){
+                    if(span.size() < 2)
+                        return Any(false);
+                    auto& a = span[0];
+                    auto& b = span[1];
+                    if(a.ptr->type() != b.ptr->type() || a.ptr->type()!=typeid(integer))
+                        return Any(false);
+                    return Any(a.as<integer>() == b.as<integer>());
+                })
+            })),
+        std::make_pair(
+            typeid(double).hash_code(),
+            std::unordered_map<std::string, function>({
+                std::make_pair("==", [](const std::span<Any>& span){
+                    if(span.size() < 2)
+                        return Any(false);
+                    auto& a = span[0];
+                    auto& b = span[1];
+                    if(a.ptr->type() != b.ptr->type() || a.ptr->type()!=typeid(double))
+                        return Any(false);
+                    return Any(a.as<double>() == b.as<double>());
+                })
+            })),
+    }
+});
+
 
 inline bool Any::operator==(const Any& a)const{
     auto& t = ptr->type();
@@ -106,4 +189,68 @@ inline bool Any::operator==(const Any& a)const{
     if(t.hash_code() == typeid(function).hash_code())
         return a.as<function>() == as<function>();
     return true;
+}
+
+inline void Any::operator=(const Any& a){
+    ptr = a.ptr;
+}
+inline void Any::operator=(const function& a){
+    *ptr = (function) a;
+};
+inline void Any::operator=(const string& a){
+    *ptr = (string) a;
+};
+inline void Any::operator=(const array& a){
+    *ptr = (array) a;
+};
+inline void Any::operator=(const map& a){
+    *ptr = (map) a;
+};
+inline bool Any::operator==(const function& a)const{
+    if(ptr->type().hash_code() == typeid(function).hash_code())
+        return as<function>() == (function) a;
+    return false;
+};
+inline bool Any::operator==(const string& a)const{
+    if(ptr->type().hash_code() == typeid(string).hash_code())
+        return as<string>() == (string) a;
+    return false;
+};
+inline bool Any::operator==(const array& a)const{
+    if(ptr->type().hash_code() == typeid(array).hash_code())
+        return as<array>() == (array) a;
+    return false;
+};
+inline bool Any::operator==(const map& a) const{
+    if(ptr->type().hash_code() == typeid(map).hash_code())
+        return as<map>() == (map) a;
+    return false;
+};
+inline Any Any::call(const string& name, array& args){
+    auto fn = any_methods.find(ptr->type().hash_code());
+    if(fn == any_methods.end())
+        return {};
+    auto it = fn->second.find(name);
+    if(it == fn->second.end() || !it->second)
+        return {};
+    args.insert(args.begin(), *this);
+    return it->second(args);
+}
+template<class ...Args>
+inline Any Any::callva(const string &name, Args ...a){
+    array arr = {Any(a)...};
+    return call(name, arr);
+}
+inline std::size_t Any::hash() const{
+    if(ptr->type() == typeid(integer)){
+        return  as<integer>();
+    }
+    if(ptr->type() == typeid(decimal)){
+        return  as<decimal>();
+    }
+    if(ptr->type() == typeid(string)){
+        std::hash<std::string> hasher;
+        return  hasher(as<string>());
+    }
+    return ptr->type().hash_code();
 }
--- v3/main.cpp
+++ v3/main.cpp
@@ -3,16 +3,16 @@
 #include <any>
 #include <iostream>
 #include <map>
+#include <ostream>
 #include <utility>
 #include "any.hpp"
 
 int main(int argc, char const *argv[])
 {
-    Any a(map({std::make_pair(Any(1), Any(2.46574657))}));
+    Any a(32);
+    Any aa(32);
+    Any b(23);
 
-    auto m = a.as<map>();
-    
-    std::cout << m[Any(1)].as<decimal>() << std::endl;
-
+    std::cout << a.callva("==", a).as<integer>() << std::endl;
     return 0;
 }
